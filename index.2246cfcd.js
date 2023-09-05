4fe6bfcd3a2f6200b3120d77fee7133a9f3e7d29
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-09-05 11:10:43 +0000

    Deploying to gh-pages from @ NataliaMahera/goit-js-hw-10@506fe033d7364cd9589c578cec693e95866db183 🚀

---
+++ index.2246cfcd.js
@@ -0,0 +1,2 @@
+fetch("".concat("https://api.thecatapi.com/v1").concat("/breeds","?api_key=").concat("live_JE40ZWz5TtBLr6X14F0x62Pn0qcsiG8Yd6dKhBir3RMHNQulDfrQKZVDqj58eqQk")).then((function(e){if(!e.ok)throw new Error(e.status);return e.json()})),document.querySelector(".breed-select").addEventListener;
+//# sourceMappingURL=index.2246cfcd.js.map
