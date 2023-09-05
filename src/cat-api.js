d83326cce7dbb957019df6cfba3d1df6b3bead86
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-09-05 15:45:49 +0200

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,13 +1,25 @@
-export const BASE_URL = 'https://api.thecatapi.com/v1';
-export const BREEDS_ENDPOINT = '/breeds';
-export const API_KEY =
+const BASE_URL = 'https://api.thecatapi.com/v1';
+const API_KEY =
   'live_JE40ZWz5TtBLr6X14F0x62Pn0qcsiG8Yd6dKhBir3RMHNQulDfrQKZVDqj58eqQk';
 
 export function fetchBreeds() {
-  fetch(`${BASE_URL}${BREEDS_ENDPOINT}?api_key=${API_KEY}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(resp => console.log(resp));
+}
+
+export function fetchCatByBreed(breedId) {
+  fetch(`${BASE_URL}images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(resp => console.log(resp));
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // axios.defaults.headers.common['x-api-key'] =
 //   'live_JE40ZWz5TtBLr6X14F0x62Pn0qcsiG8Yd6dKhBir3RMHNQulDfrQKZVDqj58eqQk';
 
-import { fetchBreeds, API_KEY, BASE_URL } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 fetchBreeds();
 
 const searchEl = document.querySelector('.breed-select');
