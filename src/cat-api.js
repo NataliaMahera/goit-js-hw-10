8620d6969a0e714db11426a2fc50363d4bb5d84e
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-09-06 16:28:45 +0200

    add

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,40 +1,14 @@
-// const BASE_URL = 'https://api.thecatapi.com/v1';
-// const API_KEY =
-//   'live_JE40ZWz5TtBLr6X14F0x62Pn0qcsiG8Yd6dKhBir3RMHNQulDfrQKZVDqj58eqQk';
-
-// export function fetchBreeds() {
-//   return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-//     return response.json();
-//   });
-//   // .then(data => console.log(data));
-// }
-
-// export function fetchCatByBreed(breedId) {
-//   return fetch(
-//     `${BASE_URL}images/search?api_key=${API_KEY}&breed_ids=${breedId}`
-//   ).then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.statusText);
-//     }
-//     return response.json();
-//   });
-// }
 const url = 'https://api.thecatapi.com/v1';
 const api_key =
   'live_JE40ZWz5TtBLr6X14F0x62Pn0qcsiG8Yd6dKhBir3RMHNQulDfrQKZVDqj58eqQk';
 
 export function fetchBreeds() {
-  return fetch(`${url}/breeds?api_key=${api_key}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(data => console.log(data));
+  return fetch(`${url}/breeds?api_key=${api_key}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
 }
 
 export function fetchCatByBreed(breedId) {
@@ -42,8 +16,11 @@ export function fetchCatByBreed(breedId) {
     `${url}/images/search?api_key=${api_key}&breed_ids=${breedId}`
   ).then(response => {
     if (!response.ok) {
-      throw new Error(response.status);
+      throw new Error(response.statusText);
     }
     return response.json();
   });
 }
+// fetchCatByBreed('crex')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import './styles.css';
 import debounce from 'lodash.debounce';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
 const select = document.querySelector('.breed-select');
 const catInfoContainer = document.querySelector('.cat-info');
@@ -12,44 +14,60 @@ const error = document.querySelector('.error');
 loader.style.visibility = 'hidden';
 error.style.visibility = 'hidden';
 
-new SlimSelect({
-  select: 'select',
-});
+let arrBreedsId = [];
+fetchBreeds()
+  .then(data => {
+    data.forEach(breed => {
+      arrBreedsId.push({ text: breed.name, value: breed.id });
+    });
+    new SlimSelect({
+      select: select,
+      data: arrBreedsId,
+    });
+  })
+  .catch(onFetchError);
+// console.log(arrBreedsId);
 
-// let arrBreedsId = [];
-// fetchBreeds()
-//   .then(data => {
-//     data.forEach(element => {
-//       arrBreedsId.push({ text: element.name, value: element.id });
-//     });
-//     new SlimSelect({
-//       select: select,
-//       data: arrBreedsId,
-//     });
-//   })
-//   .catch(onFetchError);
-
-select.addEventListener('input', debounce(onSearchBreed, 1000));
+select.addEventListener('change', debounce(onSearchBreed, 1000));
 
 function onSearchBreed(evt) {
+  Loading.standard('Loading data, please wait...', {
+    clickToClose: true,
+    svgSize: '19px',
+  });
+
   const breedId = evt.target.value;
-
-  fetchCatByBreed(breedId).then(showProfile).catch(onFetchError);
+  // console.log(evt.target);
+  fetchCatByBreed(breedId)
+    .then(data => {
+      catInfoContainer.innerHTML = renderCatMarkup(data);
+      Loading.remove();
+    })
+    .catch(onFetchError);
 }
 
-function showProfile({ image, name, description, temperament }) {
-  catInfoContainer.innerHTML = `<div class="box-img"><img src="${image}" alt="${name}" width="400"/></div><div class="box"><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`;
-}
-
-// function catCardMarkup(result) {
+// function renderCatMarkup(result) {
 //   return result
-//     .map(({ image, name, description, temperament }) => {
-//       return `< ><img src="${image}" alt="${name}" width="400"/><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`;
+//     .map(({ breeds: { name, description, temperament }, url }) => {
+//       return `<div><img src="${url}" alt="${name}" width="400"/><h1>${name}</h1><p>${description}</p><p><b>Temperament:</b> ${temperament}</p></div>`;
 //     })
 //     .join('');
 // }
-// catInfoContainer.innerHTML = catCardMarkup;
 
-function onFetchError(error) {
+function renderCatMarkup(result) {
+  return result
+    .map(({ breeds, url }) => {
+      return `<div class="box-img">
+      <img src="${url}" alt="${breeds.name}" width="400"/></div>
+      <div class="box">
+      <h1>${breeds[0].name}</h1>
+      <p>${breeds[0].description}</p>
+      <p><b>Temperament:</b> ${breeds[0].temperament}</p>
+      </div>`;
+    })
+    .join('');
+}
+
+function onFetchError() {
   Notify.failure('Oops! Something went wrong! Try reloading the page!');
 }
