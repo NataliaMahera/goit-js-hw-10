49a8ac1cf1524724911216b4af34ba5c24a23934
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-09-09 13:39:40 +0200

    update

--- src/index.js
+++ src/index.js
@@ -14,52 +14,26 @@ const error = document.querySelector('.error');
 loader.classList.add('is-hidden');
 error.classList.add('is-hidden');
 catInfoContainer.classList.add('is-hidden');
-
-// let arrBreedsId = [];
+// select.classList.add('is-hidden');
 
 fetchBreeds()
   .then(data => {
-    select.innerHTML = data
-      .map(breed => {
-        return `<option value="${breed.id}">${breed.name}</option>`;
-      })
-      .join('');
-
+    select.innerHTML = renderSelectValue(data);
     new SlimSelect({
       select: select,
       settings: {
         placeholderText: 'Search',
       },
-      //
     });
+    select.classList.remove('is-hidden');
   })
   .catch(onFetchError);
-// .finally(() => {
-//   Loading.remove();
-// });
-
-// fetchBreeds()
-//   .then(data => {
-//     data.forEach(breed => {
-//       arrBreedsId.push({ text: breed.name, value: breed.id });
-//     });
-//     new SlimSelect({
-//       settings: {
-//         placeholderText: 'Search',
-//       },
-//       select: select,
-//       data: arrBreedsId,
-//     });
-//   })
-//   .catch(onFetchError)
-//   .finally(() => {
-//     Loading.remove();
-//   });
 
 select.addEventListener('change', debounce(onSearchBreed, 1000));
 
 function onSearchBreed(evt) {
   addClassHidden();
+  loader.classList.remove('is-hidden');
   Loading.standard('Loading data, please wait...', {
     clickToClose: true,
     svgSize: '19px',
@@ -72,8 +46,18 @@ function onSearchBreed(evt) {
       catInfoContainer.innerHTML = renderCatMarkup(data);
       removeClassHidden();
       Loading.remove();
+      loader.classList.add('is-hidden');
     })
-    .catch(onFetchError);
+    .catch(onFetchError)
+    .finally(() => Loading.remove());
+}
+
+function renderSelectValue(breed) {
+  return breed
+    .map(({ id, name }) => {
+      return `<option value='${id}'>${name}</option>`;
+    })
+    .join('');
 }
 
 function renderCatMarkup(result) {
@@ -101,9 +85,89 @@ function onFetchError() {
 function addClassHidden() {
   select.classList.add('is-hidden');
   catInfoContainer.classList.add('is-hidden');
+  loader.classList.add('is-hidden');
 }
 
 function removeClassHidden() {
   select.classList.remove('is-hidden');
   catInfoContainer.classList.remove('is-hidden');
 }
+
+// fetchBreeds()
+//   .then(data => {
+//     select.innerHTML = renderListOption(data);
+//     // new SlimSelect({
+//     //   select: select,
+//     //   data: arrBreedsId,
+//     //   settings: {
+//     //     placeholderText: 'Search',
+//     //   },
+//     // });
+//   })
+//   .catch(onFetchError);
+// // .finally(() => {
+// //   Loading.remove();
+// // });
+
+// select.addEventListener('change', debounce(onSearchBreed, 1000));
+
+// function onSearchBreed(evt) {
+//   addClassHidden();
+//   loader.classList.remove('is-hidden');
+//   // Loading.standard('Loading data, please wait...', {
+//   //   clickToClose: true,
+//   //   svgSize: '19px',
+//   // });
+
+//   const breedId = evt.target.value;
+
+//   fetchCatByBreed(breedId)
+//     .then(data => {
+//       catInfoContainer.innerHTML = renderCatMarkup(data);
+//       removeClassHidden();
+//       loader.classList.add('is-hidden');
+//       // Loading.remove();
+//     })
+//     .catch(onFetchError);
+// }
+
+// function renderCatMarkup(result) {
+//   return result
+//     .map(({ breeds, url }) => {
+//       return `<div class="box-img">
+//       <img src="${url}" alt="${breeds.name}" width="500"/></div>
+//       <div class="box">
+//       <h1>${breeds[0].name}</h1>
+//       <p>${breeds[0].description}</p>
+//       <p><b>Temperament:</b> ${breeds[0].temperament}</p>
+//       </div>`;
+//     })
+//     .join('');
+// }
+
+// function renderListOption(breed) {
+//   return breed
+//     .map(({ id, name }) => {
+//       return `<option value='${id}'>${name}</option>`;
+//     })
+//     .join('');
+// }
+
+// function onFetchError() {
+//   addClassHidden();
+//   // Loading.remove();
+//   Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+//     position: 'center-top',
+//   });
+// }
+
+// function addClassHidden() {
+//   select.classList.add('is-hidden');
+//   catInfoContainer.classList.add('is-hidden');
+//   loader.classList.add('is-hidden');
+// }
+
+// function removeClassHidden() {
+//   select.classList.remove('is-hidden');
+//   catInfoContainer.classList.remove('is-hidden');
+// }
